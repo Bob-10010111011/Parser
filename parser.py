1fbec795a5de4bad4494b827456dac99921dff51
Bob-10010111011 <m37397704@gmail.com>
2026-03-13 14:33:22 +0200

    Version 1.1 — optimized HTML loading, improved file handling

--- parser.py
+++ parser.py
@@ -11,37 +11,52 @@ ctx.verify_mode = ssl.CERT_NONE
 
 url = 'https://online.dr-chuck.com/' # - source for parse
 
-def get_headers(url): # func for giving header of courses
+def opened_url(url):
     html = urlopen(url, context=ctx).read()
     soup = BeautifulSoup(html, "html.parser")
+    return soup
+
+def get_headers(soup): # func for giving header of courses
     result = [tag.get_text() for tag in soup('h3')]
     return result
 
-def get_links(url): # func for giving links on courses
-    html = urlopen(url, context=ctx).read()
-    soup = BeautifulSoup(html, "html.parser")
+def get_links(soup): # func for giving links on courses
     directory = soup.select('div.span4 > a')
     result = [i.get('href') for i in directory]
     return result
 
 
-
-links = get_links(url)
-headers = get_headers(url)
+soup = opened_url(url)
+links = get_links(soup)
+headers = get_headers(soup)
 
 print(f'Connecting to: {url} ...\n')
 
 
-# loop for outputting information
-num = 0
-while True:
-    print(f'Number of course: {num+1}')
-    print(f'Name of course: {headers[num]}')
-    print(f'Link: {links[num]}\n')
-    num += 1
-    if len(links) <= num: break
+fname = input('Enter the file name: ')+'.txt'
+
+try: # do if file exist
+    with open(fname) as f:
+        old_text = f.read()
+    with open(fname,'w') as f:
+        f.write(old_text + '\n') # rewrite file
 
 
+except FileNotFoundError: #  do if file not exist
+    print(f'File not found. Creating new file: {fname}\n')
+    with open(fname, 'w') as f:
+        pass
+
+with open(fname, 'a') as f:
+    num = 0
+    while True:
+        f.write(f'Number of course: {num + 1}\n')
+        f.write(f'Name of course: {headers[num]}\n')
+        f.write(f'Link: {links[num]}\n\n')
+        num += 1
+        if len(links) <= num: break
+
+print(f'File "{fname}" is done. You can open and read.')
 
 
 
